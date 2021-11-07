2967965472661499336519ed83712edc28d58d5b
WILLOT Philippe <willot.ph@gmail.com>
2021-11-07 12:17:51 +0000

    Added function %s, %c et %% together

---
+++ _printf.c
@@ -0,0 +1,43 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _printf - printf function
+ * @format: string
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list arglist;
+	int count = 0, i = 0, j;
+	type type[] = {
+		{"c", _printc},
+		{"s", _prints},
+		{NULL, NULL}
+		 };
+
+	va_start(arglist, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (type[j].c)
+			{
+				if (format[i + 1] == (type[j].c[0]))
+				{
+					count += type[j].ptr_f(arglist);
+					i++;
+					break;
+				}
+				j++;
+			}
+			i++;
+		}
+		write(1, &format[i], 1);
+		count++;
+		i++;
+	}
+	va_end(arglist);
+	return (count);
+}
