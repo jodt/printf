1751619128ea607738f3267b4643f82d28d4d4c6
WILLOT Philippe <willot.ph@gmail.com>
2021-11-09 20:56:19 +0000

    50000e commit

--- _printf.c
+++ _printf.c
@@ -8,23 +8,13 @@
 int _printf(const char *format, ...)
 {
 	va_list arglist;
-	int count = 0, i = 0, j = 0, count1 = 0;
-	char specifier[] = {'c', 's', 'b', 'd', 'i', 'u', 'o', 'x', 'X', 'S', 'p'};
+	int count = 0, i = 0;
 
 	va_start(arglist, format);
 
 	if (format == NULL)
 		return (-1);
 	if (format[i] == '%' && format[i + 1] == '\0')
-		return (-1);
-
-	while (specifier[j])
-	{
-		if (format[0] == '%' && format[1] == specifier[j])
-			count1++;
-		j++;
-	}
-	if (count1 == 0)
 		return (-1);
 
 	while (format && format[i] != '\0')
@@ -35,19 +25,21 @@ int _printf(const char *format, ...)
 			count++;
 			continue;
 		}
-		else if (format[i] == '%' && get_specifier_func(format[i + 1]) != NULL)
+		if (format[i] == '%')
 		{
-			count += get_specifier_func(format[i + 1])(arglist);
-			i++;
+			if (get_specifier_func(format[i + 1]))
+			{
+				count += get_specifier_func(format[i + 1])(arglist);
+				i++;
+			}
+			else if (format[i + 1] == '%')
+			{
+				write(1, &format[i + 1], 1);
+				count++, i++;
+			}
+			else
+				return (-1);
 		}
-
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			write(1, &format[i + 1], 1);
-			count++;
-			i++;
-		}
-
 		i++;
 	}
 	va_end(arglist);
