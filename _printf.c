b113e49cd185dbbf0adbd7fa7f89a70e0cf1c536
WILLOT Philippe <willot.ph@gmail.com>
2021-11-09 19:09:32 +0000

    Added else if for edge cases

--- _printf.c
+++ _printf.c
@@ -15,9 +15,6 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 
-	if (format[i] == '%' && get_specifier_func(format[i + 1]) == NULL)
-		return (-1);
-
 	while (format && format[i] != '\0')
 	{
 		if (format[i] != '%')
@@ -31,6 +28,8 @@ int _printf(const char *format, ...)
 			count += get_specifier_func(format[i + 1])(arglist);
 			i++;
 		}
+		else if (format[i] == '%' && get_specifier_func(format[i + 1]) == NULL)
+			return (-2);
 
 		if (format[i] == '%' && format[i + 1] == '%')
 		{
