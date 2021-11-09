6f9212aa3884599d671d887ae566c1d1ba07c586
WILLOT Philippe <willot.ph@gmail.com>
2021-11-09 19:39:45 +0000

    Trying to fix checks

--- _printf.c
+++ _printf.c
@@ -14,6 +14,8 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
+	if (format[i] == '%' && format[i + 1] == '\0')
+		return (-1);
 
 	while (format && format[i] != '\0')
 	{
